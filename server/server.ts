79673c824f140366dd3783a8a3f8dad69c0a6b5b
Matteo Colombo <matte.colo@gmail.com>
2019-07-26 10:31:42 +0200

    Init with ts server

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = process.env.PORT || 4200;
+const NODE_ENV = process.env.NODE_ENV || "development";
+const STATIC_DIR = path.join(__dirname, "../client");
+
+app.use("/static", express.static(STATIC_DIR));
+
+app.get("/", (req, res) => res.send("Ciao"));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
