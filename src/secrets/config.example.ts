accf184bf00ec3846183c44a1ea8a24eddec95ad
MatteoColombo <matte.colo@gmail.com>
2019-08-10 10:10:51 +0200

    update example configuration file

--- src/secrets/config.example.ts
+++ src/secrets/config.example.ts
@@ -1,5 +1,6 @@
 export const keys = {
     wca: {
+        competition_id: "",
         prod: {
             login_redirect_uri: "websitedomain/api/v0/auth/wca/callback",
             user_agent: "websitedomain",
@@ -24,7 +25,37 @@ export const keys = {
             maxAge: 1000 * 3600 * 24 * 7
         }
     },
-    cubecomps:{
-        competition_id:0,
+    game: {
+        creation_closes: "",
+        at_points: 24,
+        points: {
+            "1": 300,
+            "2": 276,
+            "3": 253,
+            "4": 231,
+            "5": 210,
+            "6": 190,
+            "7": 171,
+            "8": 153,
+            "9": 136,
+            "10": 120,
+            "11": 105,
+            "12": 91,
+            "13": 78,
+            "14": 66,
+            "15": 55,
+            "16": 45,
+            "17": 33,
+            "18": 28,
+            "19": 21,
+            "20": 15,
+            "21": 10,
+            "22": 6,
+            "23": 3,
+            "24": 1
+        }
+    },
+    cubecomps: {
+        competition_id: 0,
     }
 }
