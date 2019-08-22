defca00edf9e20988ca7d4d866c67a372fc8f542
Essay97 <saggiorato.enrico@gmail.com>
2019-08-22 19:52:03 +0200

    added homepage

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -4,4 +4,6 @@ const router: Router = Router();
 
 router.get("/", (req, res) => res.render("index", { title: "Prova", user: req.user }));
 
+router.get("/home", (req, res) => res.render("home", { title: "FantaIC", user: req.user }));
+
 export { router }
