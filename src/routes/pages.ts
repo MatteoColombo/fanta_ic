c725e709e7c2ba169579e53ac92f6c3eb92ce9d2
Essay97 <saggiorato.enrico@gmail.com>
2019-08-23 15:54:24 +0200

    added regulation page

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -6,4 +6,6 @@ router.get("/", (req, res) => res.render("index", { title: "Prova", user: req.us
 
 router.get("/home", (req, res) => res.render("home", { title: "FantaIC", user: req.user }));
 
+router.get("/regulation", (req, res) => res.render("regulation", { title: "FantaIC - Regolamento", user: req.user }));
+
 export { router }
