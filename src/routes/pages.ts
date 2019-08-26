dd522ce0dfb00ae80a9521cdb842342ff5273ec5
Essay97 <saggiorato.enrico@gmail.com>
2019-08-26 17:52:29 +0200

    add leaderboard, changed navbar, add links to home

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 
 const router: Router = Router();
 
-router.get("/", (req, res) => res.render("index", { title: "Prova", user: req.user }));
+router.get("/prova", (req, res) => res.render("index", { title: "Prova", user: req.user }));
 
 router.get("/home", (req, res) => res.render("home", { title: "FantaIC", user: req.user }));
 
-router.get("/regulation", (req, res) => res.render("regulation", { title: "FantaIC - Regolamento", user: req.user }));
+router.get("/regolamento", (req, res) => res.render("regulation", { title: "FantaIC - Regolamento", user: req.user }));
+
+router.get("/classifica", (req, res) => res.render("leaderboard", { title: "FantaIC - Classifica", user: req.user, 
+    teams: [{ name: "Ciccio", points: 24, position: 1 }, { name: "Andrea", points: 17, position: 2 },
+    { name: "Carlo", points: 5, position: 3}, {name: "Piero", points: 4, position: 4} ] }));
 
 export { router }
