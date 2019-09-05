f23691b9b0863126e236cb5df544d4f5099006bb
MatteoColombo <matte.colo@gmail.com>
2019-09-05 14:55:24 +0200

    Add import result page

--- src/public/js/importresults.js
+++ src/public/js/importresults.js
@@ -1,18 +1,27 @@
 $(document).ready(function () {
-    console.log("here");
-
-    $.getJSON("/api/results/categories/importable", function (data) {
-        console.log(data);
-        setOptions(data);
-    });
+    getOptions();
 
     function setOptions(data) {
         var select = $('#round-selector');
         select.empty();
         for (var i = 0; i < data.length; i++) {
             select.append(
-                "<option>" + data[i].name + " - Round " + (data[i].importedRounds + 1) + "</option> "
+                "<option value='/" + data[i].id + "/rounds/" + (data[i].importedRounds + 1) + "'>" + data[i].name + " - Round " + (data[i].importedRounds + 1) + "</option> "
             );
         }
     }
+
+    function getOptions(){
+        $.getJSON("/api/results/categories/importable", function (data) {
+            setOptions(data);
+        });
+    }
+
+    $('#import').click(function () {
+        var value = $('#round-selector').children("option:selected").val();
+        console.log(value);
+        $.getJSON("/api/results/import" + value, function(data){
+            getOptions();
+        });
+    });
 });
