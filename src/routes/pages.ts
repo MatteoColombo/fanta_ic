eacc6e077f712df42f816a014389d818a989bcb9
MatteoColombo <matte.colo@gmail.com>
2019-09-23 13:38:54 +0200

    Create my team page

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -23,7 +23,35 @@ router.get("/classifica", async (req, res) => {
     });
 });
 
-router.get("/crea", isLoggedInWR, async (req, res) => {
+router.get("/team", isLoggedInWR, async (req, res) => {
+    const today: Date = new Date();
+    const co = config.game.creation_opens;
+    const cc = config.game.creation_closes;
+    const creationCloses: Date = new Date(cc.year, (cc.month - 1), cc.day, cc.hour, cc.minute);
+    const creationOpens: Date = new Date(co.year, (co.month - 1), co.day, co.hour, co.minute);
+    if (today > creationCloses) {
+        let team: TeamEntity = await getCustomRepository(TeamRepository).getUserTeam(req.user.id);
+        let max: number = config.game.budget;
+        let credits: number = max - team.cubers.reduce((v, p) => v + p.price, 0);
+        let perc: number = (credits) / max * 100;
+        res.render("team", {
+            title: "Il mio Team - FantaIC",
+            user: req.user,
+            team: team._transform(),
+            perc: perc,
+            credits: credits
+        });
+    } else if (today < creationOpens) {
+        res.render("error404", { title: "Errore", user: req.user });
+    } else {
+        res.render("createteam", {
+            title: "Crea squadra - FantaIC",
+            user: req.user,
+        });
+    }
+});
+
+router.get("/team:id", isLoggedInWR, async (req, res) => {
     res.render("createteam", {
         title: "Crea squadra - FantaIC",
         user: req.user,
@@ -31,7 +59,7 @@ router.get("/crea", isLoggedInWR, async (req, res) => {
 });
 
 router.get("/login", isGuest, async (req, res) => res.render("login", {
-    title: "Crea squadra - FantaIC",
+    title: "Accedi - FantaIC",
     user: req.user,
 }));
 
